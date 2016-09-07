c61062688c1d5223c59092e9d5adbcfa64a8138d
Simon Kindström <simon.kinds@gmail.com>
2016-09-07 18:16:55 +0900

    Added fields to Sender

--- app/src/main/java/io/kindstrom/senderremote/domain/model/Sender.java
+++ app/src/main/java/io/kindstrom/senderremote/domain/model/Sender.java
@@ -1,18 +1,32 @@
 package io.kindstrom.senderremote.domain.model;
 
+import java.util.List;
+
 public class Sender {
     private final int id;
+    private final String name;
     private final String number;
     private final Pin pin;
+    private final List<Port> inputs;
+    private final List<Port> outputs;
+    private final List<Command> commands;
 
-    public Sender(int id, String number, Pin pin) {
+    public Sender(int id, String name, String number, Pin pin, List<Port> inputs, List<Port> outputs, List<Command> commands) {
         this.id = id;
+        this.name = name;
         this.number = number;
         this.pin = pin;
+        this.inputs = inputs;
+        this.outputs = outputs;
+        this.commands = commands;
     }
 
     public int getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getNumber() {
@@ -23,6 +37,18 @@ public class Sender {
         return pin;
     }
 
+    public List<Port> getInputs() {
+        return inputs;
+    }
+
+    public List<Port> getOutputs() {
+        return outputs;
+    }
+
+    public List<Command> getCommands() {
+        return commands;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -30,25 +56,23 @@ public class Sender {
 
         Sender sender = (Sender) o;
 
-        return id == sender.id && (number != null ? number.equals(sender.number) :
-                sender.number == null && (pin != null ? pin.equals(sender.pin) : sender.pin == null));
+        return id == sender.id && name.equals(sender.name) && number.equals(sender.number)
+                && pin.equals(sender.pin) && (inputs != null ? inputs.equals(sender.inputs) :
+                sender.inputs == null && (outputs != null ? outputs.equals(sender.outputs) :
+                        sender.outputs == null && (commands != null ?
+                                commands.equals(sender.commands) : sender.commands == null)));
 
     }
 
     @Override
     public int hashCode() {
         int result = id;
-        result = 31 * result + (number != null ? number.hashCode() : 0);
-        result = 31 * result + (pin != null ? pin.hashCode() : 0);
+        result = 31 * result + name.hashCode();
+        result = 31 * result + number.hashCode();
+        result = 31 * result + pin.hashCode();
+        result = 31 * result + (inputs != null ? inputs.hashCode() : 0);
+        result = 31 * result + (outputs != null ? outputs.hashCode() : 0);
+        result = 31 * result + (commands != null ? commands.hashCode() : 0);
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Sender{" +
-                "id=" + id +
-                ", number='" + number + '\'' +
-                ", pin=" + pin.getPin() +
-                '}';
     }
 }
