901eab509bf3d3775f92a235d6bbe06a487e8ba6
Simon Kindström <simon.kinds@gmail.com>
2016-09-14 11:20:51 +0900

    Added interactor for creating groups

---
+++ app/src/main/java/io/kindstrom/senderremote/domain/interactor/CreateGroupInteractor.java
@@ -0,0 +1,38 @@
+package io.kindstrom.senderremote.domain.interactor;
+
+import javax.inject.Inject;
+
+import io.kindstrom.senderremote.domain.database.GroupMemberRepository;
+import io.kindstrom.senderremote.domain.database.GroupRepository;
+import io.kindstrom.senderremote.domain.model.Group;
+import io.kindstrom.senderremote.domain.model.Sender;
+
+public class CreateGroupInteractor implements Interactor<Integer> {
+    private final GroupRepository groupRepository;
+    private final GroupMemberRepository groupMemberRepository;
+
+    private Group group;
+
+    @Inject
+    public CreateGroupInteractor(GroupRepository groupRepository, GroupMemberRepository groupMemberRepository) {
+        this.groupRepository = groupRepository;
+        this.groupMemberRepository = groupMemberRepository;
+    }
+
+    @Override
+    public Integer execute() {
+        int groupId = groupRepository.insert(group);
+        insertMembers(groupId);
+        return groupId;
+    }
+
+    private void insertMembers(int groupId) {
+        for (Sender s : group.getSenders()) {
+            groupMemberRepository.insert(groupId, s.getId());
+        }
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+}
